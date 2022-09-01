fa173f263ed9ace33db474278931c8f9f4a6803f
juancarloscamargo <juancarlos@eprinsa.es>
2022-09-01 11:50:28 +0200

    Agregar archivos de proyecto.

---
+++ CCC_EPRINSA_NetAPI/Class1.cs
@@ -0,0 +1,27 @@
+﻿using QBM.CompositionApi.Definition;
+using System;
+using VI.Base;
+
+namespace QBM.CompositionApi.EPRINSA
+{
+    public class BasicSqlMethod : IApiProvider
+    {
+        public void Build(IApiBuilder builder)
+        {
+            builder.AddMethod(Method.Define("sql/ObtenerDatosOTP")
+
+                // Insert the statement name (QBMLimitedSQL.Ident_QBMLimitedSQL) and the type
+                .HandleGetBySqlStatement("QER_CCC_Person_GetOTPOptions", SqlStatementType.SqlExecute)
+                .WithParameter("CentralAccount")
+
+                // Define the result schema columns and data types
+                .WithResultColumns(
+                new SqlResultColumn("CCC_SecondaryEmailAddress", ValType.String),
+                new SqlResultColumn("PhoneMobile", ValType.String),
+                new SqlResultColumn("CustomProperty08", ValType.String),
+                new SqlResultColumn("CustomProperty04", ValType.String)
+                ));
+
+        }
+    }
+}
