b8df995535a52523d68c9dae27fc4b934a22d601
juancarloscamargo <juancarlos@eprinsa.es>
2022-09-06 13:37:31 +0200

    2022.09.06.02

--- CCC_EPRINSA_NetAPI/Class1.cs
+++ CCC_EPRINSA_NetAPI/Class1.cs
@@ -1,16 +1,26 @@
 ﻿using QBM.CompositionApi.Definition;
+using QBM.CompositionApi.Api;
+using QBM.CompositionApi.ApiManager;
+using QBM.CompositionApi.Cache;
+using QBM.CompositionApi.Captcha;
+using QBM.CompositionApi.Config;
+using QBM.CompositionApi.Definition;
+using QBM.CompositionApi.Handling;
+using QBM.CompositionApi.PlugIns;
+using QER.CompositionApi.Config;
+using QER.CompositionApi.Person;
 
 using System;
 using VI.Base;
+
 using QBM.CompositionApi.ApiManager;
 using QER.CompositionApi.Portal;
-
-
-
+using QER.CompositionApi.Password;
 
 namespace QBM.CompositionApi
+
 {
-    public class Eprinsa : IApiProviderFor<PortalApiProject>
+    public class Eprinsa : IApiProviderFor<PasswordPortalApiProject>
     {
         public void Build(IApiBuilder builder)
         {
