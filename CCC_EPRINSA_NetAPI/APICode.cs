690f466dee00d23e2237710cafd48b3033823736
juancarloscamargo <juancarlos@eprinsa.es>
2022-09-28 13:19:41 +0200

    2022.09.28.03 Pruebas autenticación

---
+++ CCC_EPRINSA_NetAPI/APICode.cs
@@ -0,0 +1,49 @@
+﻿using QBM.CompositionApi.Definition;
+using VI.Base;
+using QBM.CompositionApi.ApiManager;
+
+public class Eprinsa : IApiProvider
+{
+    public void Build(IApiBuilder builder)
+    {
+
+
+
+        builder.AddMethod(Method.Define("ObtenerDatosOTP")
+            // Insert the statement name (QBMLimitedSQL.Ident_QBMLimitedSQL) and the type
+            .AllowUnauthenticated()
+            .HandleGetBySqlStatement("QER_CCC_Person_GetOTPOptions", SqlStatementType.SqlExecute)
+            .WithParameter("CentralAccount")
+            
+
+            // Define the result schema columns and data types
+            .WithResultColumns(
+            new SqlResultColumn("CCC_SecondaryEmailAddress", ValType.String),
+            new SqlResultColumn("PhoneMobile", ValType.String),
+            new SqlResultColumn("CustomProperty08", ValType.String),
+            new SqlResultColumn("CustomProperty04", ValType.String)
+            ));
+
+        builder.AddMethod(Method.Define("helloworld")
+           .AllowUnauthenticated()
+           .HandleGet(qr => new DataObject { Message = "Hello world!" }));
+
+        builder.AddMethod(Method.Define("testvalidation")
+            .AllowUnauthenticated()
+            .WithParameter("p", description: "Parameter must be 'a' or 'b'")
+            .WithValidator("Parameter validation", qr =>
+                {
+                    var value = qr.Parameters.Get<string>("p");
+                    if (value != "a" && value != "b")
+                    return new ValidationError("Invalid value for parameter");
+                return null;
+                })
+            .HandleGet(qr => new DataObject { Message = "Hello world!" }));
+
+
+    }
+}
+public class DataObject
+{
+    public string Message { get; set; }
+}
---
+++ CCC_EPRINSA_NetAPI/APIDef.cs
@@ -0,0 +1,66 @@
+﻿using QBM.CompositionApi.Definition;
+
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VI.Base;
+
+
+using QBM.CompositionApi.ApiManager;
+
+using QBM.CompositionApi.Captcha;
+using QBM.CompositionApi.Config;
+using QBM.CompositionApi.Session;
+using System.Reflection;
+using System;
+
+
+namespace QBM.CompositionApi
+{
+    public class PluginMethodSetProvider : IPlugInMethodSetProvider
+    {
+        public IMethodSetProvider Build(IResolve resolver)
+        {
+            return new ApiSamples(resolver);
+        }
+    }
+
+    internal class ApiSamples : IMethodSetProvider
+    {
+        private readonly IResolve _resolver;
+
+        public ApiSamples(IResolve resolver)
+        {
+            _resolver = resolver;
+        }
+
+        public Task<IEnumerable<IMethodSet>> GetMethodSetsAsync(CancellationToken ct = new CancellationToken())
+        {
+            var methodSet = new MethodSet
+            {
+                AppId = "apieprinsa",
+                SessionConfig = new SessionAuthDbConfig {
+                    AuthenticationType = AuthType.Default,
+                    AllowPersistentAuth = true
+                    
+                }
+            };
+
+            // Include all classes in this assembly that implement IApiProvider
+            methodSet.Configure(_resolver, Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => typeof(IApiProvider).IsAssignableFrom(t))
+                .OrderBy(t => t.FullName)
+                .Select(t => (IApiProvider)t.GetConstructor(new Type[0]).Invoke(new object[0])));
+
+            return Task.FromResult<IEnumerable<IMethodSet>>(new[]
+            {
+                methodSet
+            });
+        }
+    }
+}
+
+
+
