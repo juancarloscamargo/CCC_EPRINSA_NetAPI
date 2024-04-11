174c431f35539bf3e8a095f69160bea4dcab2285
Juan Carlos Camargo Carrillo <juancarlos@eprinsa.es>
2024-04-11 12:05:45 +0200

    Incorporación del api de gappasku

--- CCC_EPRINSA_NetAPI/APICode.cs
+++ CCC_EPRINSA_NetAPI/APICode.cs
@@ -1,11 +1,11 @@
-﻿using QBM.CompositionApi.Definition;
+﻿
 
 using VI.Base;
-using QBM.CompositionApi.ApiManager;
+
 using QBM.CompositionApi.Crud;
 using VI.DB.Entities;
-using QBM.CompositionApi;
-using QBM.CompositionApi.Definition;
+
+
 using System.Linq;
 using System.Threading.Tasks;
 using VI.DB;
@@ -20,50 +20,58 @@ using System.Reflection.Emit;
 using VI.DB.JobGeneration;
 using System.Collections.Generic;
 
-public class APIEprinsaRest : IApiProviderFor<QER.CompositionApi.Portal.PortalApiProject>
+using QBM.CompositionApi.ApiManager;
+using QBM.CompositionApi.Definition;
+
+[assembly: QBM.CompositionApi.PlugIns.Module("CCC")]
+
+namespace QBM.CompositionApi
 {
-    public void Build(IApiBuilder builder)
+    public class APIEprinsaRest : IApiProviderFor<QER.CompositionApi.Portal.PortalApiProject>
     {
+        public void Build(IApiBuilder builder)
+        {
 
 
 
-        
 
 
-        builder.AddMethod(Method.Define("solicitudotp")
-                .AllowUnauthenticated(true)
-                .WithParameter("OTP_Usuario",typeof(string),isInQuery:true)
-                .WithParameter("OTP_Metodo_Dato",typeof(string),isInQuery:true)
-                .HandleGet(qr =>
-                {
-                    // Setup the script runner
-                    var scriptClass = qr.Session.Scripts().GetScriptClass(ScriptContext.Scripts);
-                    var runner = new ScriptRunner(scriptClass, qr.Session);
 
-                    // Add any script input parameters to this array.
-                    // In this example, the script parameters are defined as
-                    // URL parameters, and their values must be supplied
-                    // by the client. This does not have to be the case.
-                    var parameters = new object[]
+            builder.AddMethod(Method.Define("solicitudotp")
+                    .AllowUnauthenticated(true)
+                    .WithParameter("OTP_Usuario", typeof(string), isInQuery: true)
+                    .WithParameter("OTP_Metodo_Dato", typeof(string), isInQuery: true)
+                    .HandleGet(qr =>
                     {
+                        // Setup the script runner
+                        var scriptClass = qr.Session.Scripts().GetScriptClass(ScriptContext.Scripts);
+                        var runner = new ScriptRunner(scriptClass, qr.Session);
+
+                        // Add any script input parameters to this array.
+                        // In this example, the script parameters are defined as
+                        // URL parameters, and their values must be supplied
+                        // by the client. This does not have to be the case.
+                        var parameters = new object[]
+                        {
                         qr.Parameters.Get<string>("OTP_Usuario"),
                         qr.Parameters.Get<string>("OTP_Metodo_Dato"),
 
-                    };
-
-                    
-                    return runner.Eval("CCC_EPRINSA_RespondeSolicitudOTP91", parameters) as string;
-                })); ;
+                        };
 
 
-        builder.AddMethod(Method.Define("GAPUserLicense")
-            .FromTable("GAPUserInPaSku")
-            .EnableRead()
-            .WithAllColumns());
-              
-                
+                        return runner.Eval("CCC_EPRINSA_RespondeSolicitudOTP91", parameters) as string;
+                    })); ;
 
-        
 
+            builder.AddMethod(Method.Define("targetsystem/gappasku/GAPUserLicense")
+                .FromTable("GAPUserInPaSku")
+                .EnableRead()
+                .WithAllColumns());
+
+
+
+
+
+        }
     }
 }
